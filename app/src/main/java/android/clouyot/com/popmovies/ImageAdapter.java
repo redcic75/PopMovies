eea887d64c1ee2f4591cc4f9510f990e8f48ff83
Cédric Louyot <cedric.louyot@gmail.com>
2015-12-18 08:07:08 +0100

    GridView d'image utilisant ImageAdapter extends BaseAdapter

--- app/src/main/java/android/clouyot/com/popmovies/ImageAdapter.java
+++ app/src/main/java/android/clouyot/com/popmovies/ImageAdapter.java
@@ -1,28 +1,60 @@
 package android.clouyot.com.popmovies;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter {
 
-    @Override
-    public int getCount() {
-        return 0;
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
     }
 
-    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
     public Object getItem(int position) {
         return null;
     }
 
-    @Override
     public long getItemId(int position) {
         return 0;
     }
 
-    @Override
+    // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
     }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
 }
+
+
--- app/src/main/java/android/clouyot/com/popmovies/MovieGridFragment.java
+++ app/src/main/java/android/clouyot/com/popmovies/MovieGridFragment.java
@@ -6,12 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.GridView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MovieGridFragment extends Fragment {
 
@@ -22,25 +17,11 @@ public class MovieGridFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        List<String> movieList = new ArrayList<String>(Arrays.asList(
-                "Le bonheur est dans le pré",
-                "Armaggedon",
-                "La vie est belle",
-                "Suits",
-                "Pour un dollar de plus"));
-
-        ArrayAdapter<String> mMovieGridAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                R.layout.list_item_movie,
-                R.id.list_item_movie_element,
-                movieList
-        );
-
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_movie_grid, container, false);
 
         GridView mGridView = (GridView) rootView.findViewById(R.id.movie_grid_view);
-        mGridView.setAdapter(mMovieGridAdapter);
+        mGridView.setAdapter(new ImageAdapter(getContext()));
 
         return rootView;
     }
